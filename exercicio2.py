67d949d8ba77137b55012fb7b2740e7af470c797
Nicolas <nicolasandrade.de.freitas@gmail.com>
2021-09-09 14:37:10 -0300

    Create exercicio2.py

---
+++ exercicio2.py
@@ -0,0 +1,17 @@
+lista_real = [3.5, 10.4, 0.7]
+print(lista_real)
+
+lista_real.append(5.5)
+lista_real.append(2.2)
+print(lista_real)
+
+lista_real.insert(1, 4.78)
+lista_real.insert(4, 8.98)
+lista_real.append(100.10)
+print(lista_real)
+
+lista_real.append(5.10)
+lista_real.append(3.87)
+print(lista_real)
+
+print("Lista com as posições reversas ", list(reversed(lista_real)))
