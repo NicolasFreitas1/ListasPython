04b6d3412afe07e9cdc0537263311bef546b450f
Nicolas <nicolasandrade.de.freitas@gmail.com>
2021-09-09 15:11:53 -0300

    Update exercicio6.py

--- exercicio6.py
+++ exercicio6.py
@@ -1,4 +1,13 @@
-lista_nomes = ["Nicolas", "Lucas", "Giordano"]
-print(lista_nomes)
+e1 = input("Digite o primeiro elemento: ")
+e2 = input("Digite o segundo elemento: ")
+e3 = input("Digite o terceiro elemento: ")
 
-print("Nicolas esta na posição: ",lista_nomes.index("Nicolas"))
+lista = [e1,e2,e3]
+print(lista)
+
+e = print("Qual elemento voce quer saber a posição da lista: ")
+
+if e in lista:
+    print("{} esta na posição: ",lista.index(e), .format(e))
+else:
+    print("{} nao está na lista ".format(e))
