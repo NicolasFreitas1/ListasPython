29f78156093b1773c06c83afea8bcbc043c57842
Nicolas <nicolasandrade.de.freitas@gmail.com>
2021-09-09 14:26:20 -0300

    Create exercicio1.py

---
+++ exercicio1.py
@@ -0,0 +1,18 @@
+lista_num = [1]
+print(lista_num)
+lista_num.append(2)
+print(lista_num)
+lista_num.insert(1, 5)
+print(lista_num)
+lista_num.insert(3, 8)
+lista_num.append(3)
+print(lista_num)
+
+n1 = input("Digite o Primeiro numero: ")
+n2 = input("Digite o Segundo numero: ")
+n3 = input("Digite o Terceiro numero: ")
+n4 = input("Digite o Quarto numero: ")
+n5 = input("Digite o Quinto numero: ")
+
+lista_num2 = [n1,n2,n3,n4,n5]
+print(lista_num2)
