47bfdd9d70db0e1838bf6285fc12e5ddf1553a37
Nicolas <nicolasandrade.de.freitas@gmail.com>
2021-09-09 15:11:50 -0300

    Update exercicio5.py

--- exercicio5.py
+++ exercicio5.py
@@ -1,6 +1,12 @@
-lista_nomes = ["Nicolas", "Lucas", "Giordano"]
 
-if "Nicolas" in lista_nomes:
-    print("Nicolas esta na lista!")
+nome1 = input("Digite o primeiro nome: ")
+nome2 = input("Digite o primeiro nome: ")
+nome3 = input("Digite o primeiro nome: ")
+
+lista_nomes = [nome1, nome2, nome3]
+
+nome = input("Qual nome voce quer saber se existe na lista? ")
+if nome in lista_nomes:
+    print("{} esta na lista!".format(nome))
 else:
-    print("Nicolas nao esta na lista :(")    
+    print("{} nao esta na lista :(".format(nome))    
