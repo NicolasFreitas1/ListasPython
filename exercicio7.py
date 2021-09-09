00fa18695569ec5c89d625cc638541fc689fdfbf
Nicolas <nicolasandrade.de.freitas@gmail.com>
2021-09-09 14:57:08 -0300

    Create exercicio7.py

---
+++ exercicio7.py
@@ -0,0 +1,4 @@
+lista = ["Banana", "Maçã", "Banana", "Pera"]
+
+print("Quantidade de vezesd que repetiu banana: ", lista.count("Banana"))
+print("Quantidade de vezesd que repetiu banana: ", lista.count("Maçã"))
