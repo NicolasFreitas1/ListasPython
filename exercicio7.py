4d85c1988e348e3d66f8ab3d87f35d77b6b94e8c
Nicolas <nicolasandrade.de.freitas@gmail.com>
2021-09-09 14:57:53 -0300

    Update exercicio7.py

--- exercicio7.py
+++ exercicio7.py
@@ -1,4 +1,5 @@
 lista = ["Banana", "Maçã", "Banana", "Pera"]
 
 print("Quantidade de vezesd que repetiu banana: ", lista.count("Banana"))
-print("Quantidade de vezesd que repetiu banana: ", lista.count("Maçã"))
+print("Quantidade de vezesd que repetiu Maçã: ", lista.count("Maçã"))
+print("Quantidade de vezesd que repetiu Pera: ", lista.count("Pera"))
